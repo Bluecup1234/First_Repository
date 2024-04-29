07fce772e349c4810010f0c1cd140f3ccb3f7b73
Bluecup1234 <ugochukwuonah6@gmail.com>
2024-04-29 14:33:45 +0100

    mod added

---
+++ mod.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+data = pd.read_csv('C:/Users/BLUE-CORE/Desktop/modules/football_players.csv', low_memory=False)
+
+print(data.head(10))
+print(data.tail(20))
+
+
+
+
+# How to use Modules in Data Analysis
+# search for documentations on:
+
+# pandas
+# matplotlib
+# numpy
