1ff2160c1a512aeae1962edaa47ca85031366e5e
Bluecup1234 <ugochukwuonah6@gmail.com>
2024-05-08 12:26:05 +0100

    adding student

---
+++ coco.py
@@ -0,0 +1,42 @@
+Name = "The students name is Tony"
+Age = "He is 16 years old"
+Gender = "He is a Male"
+Class = "His class is Year 4"
+
+ print(Name, Age, Gender, Class)
+
+student = dict()
+
+student["Name"] = "The students name is Tony"
+student["Age"] = "He is 16 years old"
+student["Gender"]= "He is a Male"
+student["Class"] = "His class is Year 4"
+
+ print(student.items())
+
+
+
+
+
+
+
+
+
+name = input("Enter your name: ")
+age = int(input("How old are you: "))
+Class = input("What class are you: ")
+gender = input("What gender are you: ")
+
+name = "Perry"
+age = 25
+Class = "Firts Class"
+gender = "Male"
+
+student = dict()
+
+student["name"] = name
+student["age"] = age
+student["Class"] = Class
+student["gender"] = gender
+
+ print(student.items())
